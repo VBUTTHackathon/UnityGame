aaab22d9718add830eb65ec7c18ff677f0bb0b15
ines eleuch <ines.eleuch@gmail.com>
2016-11-18 15:29:55 +0100

    Character with animation and respective scripts in c#

---
+++ Avatar/Assets/scripts/PlayerMovements.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovements : MonoBehaviour {
+
+	Rigidbody2D rbody;
+	Animator anim;
+	// Use this for initialization
+	void Start () {
+		rbody = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 mvm_vector = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+
+		if (mvm_vector != Vector2.zero) {
+			anim.SetBool ("iswalking", true);
+			anim.SetFloat ("input_x", mvm_vector.x);
+			anim.SetFloat ("input_y", mvm_vector.y);
+		} else {
+			anim.SetBool ("iswalking", false);
+		}
+
+		rbody.MovePosition (rbody.position + mvm_vector * Time.deltaTime);
+	}
+}
