f2557000cbdbfecb5c7f03f098f17d4d37a9e6ef
NabilLDH <laadhari.nabil@gmail.com>
2016-11-20 00:03:53 +0100

    first commit, map creation

--- Assets/Tiled2Unity/Scripts/Move Script.js
+++
@@ -1,8 +0,0 @@
-﻿
-function Update() {
-    // Move the object forward along its z axis 1 unit/second.
-    if (Input.GetKey(KeyCode.UpArrow)) transform.Translate(Vector3(0, 1, 0) * Time.deltaTime);
-    if (Input.GetKey(KeyCode.DownArrow)) transform.Translate(Vector3(0, -1, 0) * Time.deltaTime);
-    if (Input.GetKey(KeyCode.LeftArrow)) transform.Translate(Vector3(-1, 0, 0) * Time.deltaTime);
-    if (Input.GetKey(KeyCode.RightArrow)) transform.Translate(Vector3(1, 0, 0) * Time.deltaTime);
-}
